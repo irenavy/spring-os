6c8538950350db334b3ff9f64d7797dae034543f
aneri.vyas <vyas.aneri@gmail.com>
2022-05-11 20:02:03 +0530

    first commit

---
+++ src/main/java/com/example/springos/SpringOsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOsApplication.class, args);
+	}
+
+}
