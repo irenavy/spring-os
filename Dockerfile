12e7d0f839ca7ea5dc50eb4c90914407f32d4462
aneri.vyas <vyas.aneri@gmail.com>
2022-05-12 19:26:48 +0530

    dockerfile added

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# Start with a base image containing Java runtime
+
+FROM adoptopenjdk/openjdk11:alpine-jre
+
+
+# Add a volume pointing to /tmp
+VOLUME /tmp
+
+# The application's jar file
+ARG JAR_FILE=target/spring-os-0.0.1-SNAPSHOT.jar
+
+# Add the application's jar to the container
+ADD ${JAR_FILE} spring-os-0.0.1-SNAPSHOT.jar
+
+# Run the jar file
+ENTRYPOINT ["java","/spring-os-0.0.1-SNAPSHOT.jar"]
---
+++ src/main/java/com/example/springos/controller/GreetingController.java
@@ -0,0 +1,19 @@
+package com.example.springos.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	
+	@Value("${greeting.message}")
+	private String message;
+
+	@GetMapping("/greeting")
+	public String greeting(@RequestParam(value = "name", defaultValue = "Hello World") String name) {
+		return message;
+	}
+
+}
