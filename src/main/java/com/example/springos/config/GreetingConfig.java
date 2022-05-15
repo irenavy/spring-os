eafef4e12fc7e6319afc9b411c63be619d640738
aneri.vyas <vyas.aneri@gmail.com>
2022-05-15 21:17:51 +0530

    fixed

--- src/main/java/com/example/springos/config/GreetingConfig.java
+++ src/main/java/com/example/springos/config/GreetingConfig.java
@@ -1,6 +1,7 @@
 package com.example.springos.config;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @ConfigurationProperties(prefix="greetingtwo")
